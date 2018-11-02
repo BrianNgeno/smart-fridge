42c07cd03eb62fe5fa76942039cfd077501cea97
brianngeno <bkn.ngeno@gmail.com>
2018-11-02 17:29:40 +0300

    updated store

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,7 @@ from . import api,views
 urlpatterns = [
     # ... the rest of your URLconf goes here ...
     url(r'^$',views.home_page, name='home'),
+    url(r'^order$',views.order, name='order'),
     url(r'^notify$', api.realtime_notify),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from fridge.models import Cart
 
 # Create your views here.
 def home_page(request):
     return render(request,'home.html')
+
+def order(request):
+    items = Cart.objects.filter(paid='False')
+    prices = []
+    for item in items:
+       price = int(item.item.price)
+       prices.append(price)
+    print(prices)
+    total = sum(prices)
+    return render(request,'order.html',locals())
